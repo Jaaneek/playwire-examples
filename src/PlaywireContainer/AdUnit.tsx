16e6d255fef0863c677dea832f2ff769cdaefa1c
Jaaneek <jankiewiczmilosz@gmail.com>
2022-11-16 16:51:01 +0100

    display div

--- src/PlaywireContainer/AdUnit.tsx
+++ src/PlaywireContainer/AdUnit.tsx
@@ -14,13 +14,5 @@ export const AdUnit = (props: IProps) => {
   else if (type === "square-two") useName = "responsive-bottom-square";
   else if (type === "skyscraper-one") useName = "responsive-top-skyscraper";
   else if (type === "skyscraper-two") useName = "responsive-bottom-skyscraper";
-  else if (useName.includes("leaderboard"))
-    return (
-      <div className="w-64 sm:w-80 md:w-92 lg:w-128 xl:w-164 mx-auto rounded-lg bg-gray-700 h-16" />
-    );
-  else if (useName.includes("square"))
-    return <div className="w-64 mx-auto rounded-lg bg-gray-700 h-48" />;
-  else if (useName.includes("skyscraper"))
-    return <div className="w-64 mx-auto rounded-lg bg-gray-700 h-128" />;
-  return <></>;
+  return <div id={useName} className="pw pwUnit" />;
 };
